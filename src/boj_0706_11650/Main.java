7a6de21dc42cbe270d4e0a6ac224cf9cc40f86c7
syjdjr <syjdjr@naver.com>
2024-07-06 23:49:56 +0900

    feat: 11650번 좌표 정렬하기

---
+++ src/boj_0706_11650/Main.java
@@ -0,0 +1,20 @@
+package boj_0706_11650;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        setting();
+        solve();
+    }
+
+    public static void setting() {
+
+    }
+
+    public static void solve() {
+
+    }
+}
