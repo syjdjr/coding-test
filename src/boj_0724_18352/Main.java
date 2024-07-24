b5878834502b192f1f0013f431562e7ca754ba14
syjdjr <81100851+syjdjr@users.noreply.github.com>
2024-07-24 19:45:48 +0900

    Create Main.java
    feat: 18352번 특정 거리의 도시 찾기

---
+++ src/boj_0724_18352/Main.java
@@ -0,0 +1,17 @@
+package boj_0724_18352;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    static void setting() {
+        
+    }
+    static void solve() {
+        
+    }
+}
