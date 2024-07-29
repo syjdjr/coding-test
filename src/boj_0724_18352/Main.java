18faa0e36e6a9b2007e5d62f5b44fa7f8e28b216
syjdjr <syjdjr@naver.com>
2024-07-29 19:16:14 +0900

    feat: 18352번 수정

--- src/boj_0724_18352/Main.java
+++ src/boj_0724_18352/Main.java
@@ -4,12 +4,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static int N, M, K, X;
-    private static List<Node> map = new LinkedList<>();
-    private static boolean[] isVisit;
-    private static Queue<Integer> q = new LinkedList<>();
 
-    public static void main() throws IOException {
+    private static int N, M, K, X;
+    private static List<Node>[] map;
+    private static boolean[] visited;
+    private static int[] dist;
+
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
         setting();
         solve();
     }
@@ -18,12 +21,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());   //도시의 개수
-        M = Integer.parseInt(st.nextToken());   //도로의 개수
-        K = Integer.parseInt(st.nextToken());   //거리
-        X = Integer.parseInt(st.nextToken());   //출발 도시의 번호
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        isVisit = new boolean[M];
+        map = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        dist = new int[N + 1];
+
+        for (int i = 0; i <= N; i++){
+            map[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -31,31 +40,63 @@ public class Main {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
+            map[A].add(new Node(B,0));
         }
     }
 
-    static void solve() {
+    public static void solve() {
+        Queue<Node> q = new PriorityQueue<>();
 
-        q.add(X);
-        isVisit[X] = true;
+        Node node = new Node(X, 0);
+        q.add(node);
+        visited[X] = true;
+        dist[X] = 0;
+        int distance = 0;
 
-        for(int i = 0; i < K; i++) {
+        while (!q.isEmpty()) {
+            Node vertex = q.poll();
+            int nowNode = vertex.edge;
+            int nowDist = vertex.dist;
 
-            while(!q.isEmpty()) {
-                int vertex = q.poll();
+            if (nowDist == K) {
+                sb.append(nowNode).append("\n");
+            }
 
+            for (Node next : map[nowNode]) {
+                int nextNode = next.edge;
+                int nextDist = distance + next.dist;
+
+                if (nextDist < dist[nextNode]) {
+                    dist[nextNode] = nextDist;
+                    if (!visited[nextNode]) {
+                        visited[nextNode] = true;
+                        q.add(new Node(nextNode, nextDist));
+                    }
+                }
             }
         }
+
+
+        if (sb.isEmpty()) {
+            sb.append(-1);
+        }
+
+        System.out.println(sb);
     }
 
-    static class Node {
-        int value, dist;
 
-        public Node(int value, int dist) {
-            this.value = value;
+    static class Node implements Comparable<Node> {
+        int edge, dist;
+
+        public Node(int edge, int dist) {
+            this.edge = edge;
             this.dist = dist;
         }
+
+        @Override
+        public int compareTo(Node node) {
+
+            return this.dist - node.dist;
+        }
     }
-
 }
-
