758125060a6fd44788ac8181e9199b1d07ca2c07
syjdjr <syjdjr@naver.com>
2024-08-09 15:20:46 +0900

    feat: 16928번 뱀과 사다리 게임

---
+++ src/boj_0809_16928/Main.java
@@ -0,0 +1,19 @@
+package boj_0809_16928;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        setting();
+        solve();
+    }
+
+    static void setting() {
+
+    }
+
+    static void solve() {
+
+    }
+}
