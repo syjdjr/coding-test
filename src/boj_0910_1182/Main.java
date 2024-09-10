54ad385cac5f6f947e9444f25e18a95034afe6e2
syjdjr <syjdjr@naver.com>
2024-09-10 13:34:18 +0900

    feat: 1182번 부분수열의 합

---
+++ src/boj_0910_1182/Main.java
@@ -0,0 +1,27 @@
+package boj_0910_1182;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, S;
+
+    public static void main(String[] args) throws IOException {
+        setting();
+        solve();
+    }
+
+    private static void setting() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+
+    }
+
+    private static void solve(int count) {
+        if (count == S)
+    }
+}
