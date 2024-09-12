2c429a7c19ff8932e6f06c2bda0f391a49910df3
syjdjr <syjdjr@naver.com>
2024-09-12 15:56:00 +0900

    feat: 1182번 부분수열의 합

--- src/boj_0910_1182/Main.java
+++ src/boj_0910_1182/Main.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class Main {
     private static int N, S;
+    private static int count = 0;
+    private static List<Integer> data = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         setting();
@@ -18,10 +20,29 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
 
-
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            data.add(Integer.parseInt(st.nextToken()));
+        }
     }
 
-    private static void solve(int count) {
-        if (count == S)
+    private static void solve() {
+        tracking(0,0);
+
+        if (S == 0) {
+            count--;
+        }
+        System.out.println(count);
+    }
+
+    private static void tracking(int start, int sum) {
+
+        if (sum == S) {
+            count++;
+        }
+
+        for (int i = start; i < data.size(); i++) {
+            tracking(i + 1, sum + data.get(i));
+        }
     }
 }
