5b2f5926085c36c3dc566e4cd42ae49b749be8a7
syjdjr <syjdjr@naver.com>
2025-01-04 16:03:48 +0900

    refactor: 154539번 뒤에 있는 큰수 찾기

--- src/prg_0104_154539/Solution2.java
+++ src/prg_0104_154539/Solution2.java
@@ -2,26 +2,24 @@ package prg_0104_154539;
 
 import java.util.*;
 
-class Solution2 {
+class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
         Arrays.fill(answer, -1);
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((nowNum, newNum) -> nowNum[0] - newNum[0]);
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((nowNum, newNum) -> nowNum[1] - newNum[1]);
 
         for (int i = 0; i <  numbers.length; i++) {
-            while (!minHeap.isEmpty() && minHeap.peek()[0] < numbers[i]) {
+            while (!minHeap.isEmpty() && minHeap.peek()[1] < numbers[i]) {
                 int[] top = minHeap.poll();
-                answer[top[1]] = numbers[i];
+                answer[top[0]] = numbers[i];
             }
 
-            int[] element = new int[2];
-            element[0] = numbers[i];
-            element[1] = i;
+            int[] node = new int[2];
+            node[0] = i;
+            node[1] = numbers[i];
 
-            minHeap.offer(element);
-
-            //minHeap.offer(new int[]{numbers[i], i});
+            minHeap.offer(node);
         }
 
         return answer;
