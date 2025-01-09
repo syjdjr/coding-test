e614392a8180e0bb943b50b8c3c7e6528514a7bf
syjdjr <syjdjr@naver.com>
2025-01-09 15:40:10 +0900

    feat: 2606번 바이러스 문제

--- src/boj_0109_2606/Main.java
+++ src/boj_0109_2606/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     private static int N;   //컴퓨터의 수
     private static int M;   //컴퓨터 쌍의 수
-    private static boolean[][] map;
+    private static List<List<Integer>> list = new ArrayList<>();
     private static boolean[] isVisit;
 
     public static void main(String[] args) throws IOException {
@@ -14,14 +14,17 @@ public class Main {
         solve();
     }
 
-    public static void setting() throws IOException {
+    private static void setting() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        map = new boolean[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
         isVisit = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
@@ -30,33 +33,30 @@ public class Main {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            map[A][B] = true;
-            map[B][A] = true;
+            list.get(A).add(B);
+            list.get(B).add(A);
         }
     }
 
-    public static void solve() {
+    private static void solve() {
         Queue<Integer> q = new LinkedList<>();
+        int count = 0;
         isVisit[1] = true;
 
-        int count = 0;
-
-        for (int i = 1; i <= N; i++) {
-            if (map[1][i] && !isVisit[i]) {
-                q.offer(i);
-                isVisit[i] = true;
-            }
+        for (int i : list.get(1)) {
+            q.offer(i);
+            isVisit[i] = true;
+            count += 1;
         }
 
         while (!q.isEmpty()) {
             int now = q.poll();
 
-            isVisit[now] = true;
-            count += 1;
-
-            for (int j = 1; j <= N; j++) {
-                if (map[now][j] && !isVisit[j]) {
-                    q.offer(j);
+            for (int i : list.get(now)) {
+                if (!isVisit[i]) {
+                    q.offer(i);
+                    isVisit[i] = true;
+                    count += 1;
                 }
             }
         }
